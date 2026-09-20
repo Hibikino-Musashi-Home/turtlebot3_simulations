e9181ff30a72c747fdd4d46868fbb2f6de0854b1
Tomoaki Fujino <fujino.tomoaki404@mail.kyutech.jp>
2026-09-20 12:48:09 +0900

    fix: use sensor data QoS for the Gazebo image bridge
    Signed-off-by: Tomoaki Fujino <fujino.tomoaki404@mail.kyutech.jp>

--- turtlebot3_gazebo/launch/spawn_turtlebot3.launch.py
+++ turtlebot3_gazebo/launch/spawn_turtlebot3.launch.py
@@ -79,6 +79,7 @@ def generate_launch_description():
         package='ros_gz_image',
         executable='image_bridge',
         arguments=['/camera/image_raw'],
+        parameters=[{'qos': 'sensor_data'}],
         output='screen',
     )
     ld = LaunchDescription()
